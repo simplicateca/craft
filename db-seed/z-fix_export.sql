abdd253eaeca028c0e7c9ca9e532f08aa7ed01fb
Steve Comrie <steve@simplicate.ca>
2022-05-15 23:40:26 -0400

    Remove root user fix from mysql start-up

--- db-seed/z-fix_export.sql
+++
@@ -1,6 +0,0 @@
--- this code fixes an error that was occuring when attempting to export using mysqldump
--- it has something to do with a change in the password encryption on mysql8
--- https://stackoverflow.com/questions/49194719/authentication-plugin-caching-sha2-password-cannot-be-loaded
--- it should only need to run once, and should trigger automatically when the mysqlserver8 docker container first builds
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
-ALTER USER 'project'@'%' IDENTIFIED WITH mysql_native_password BY 'project';
