b9536f37a93273c637497c2a86f69925153424c4
Steve Comrie <steve@simplicate.ca>
2022-05-16 00:13:56 -0400

    Trying to switch back to MariaDB with a forced "Analyze Tables" query after the import.
    Per: https://github.com/MariaDB/mariadb-docker/issues/211#issuecomment-450251171

--- db-seed/z-fix-mysql8.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# this code fixes an error that was occuring when attempting to export using mysqldump
-# it has something to do with a change in the password encryption on mysql8
-# https://stackoverflow.com/questions/49194719/authentication-plugin-caching-sha2-password-cannot-be-loaded
-# it should only need to run once, and should trigger automatically when the mysqlserver8 docker container first builds
-
-mysql -u root -psecret -Bse "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';ALTER USER 'project'@'%' IDENTIFIED WITH mysql_native_password BY 'project';"
